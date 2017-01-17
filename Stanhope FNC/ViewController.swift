a5674f3bbb1fbcf104ed053eb70808ebca7345fc
Jason Leocata <jasonleocata@jasons-imac.local>
2017-01-17 21:15:40 +1100

    Initial Commit

---
+++ Stanhope FNC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stanhope FNC
+//
+//  Created by Jason Leocata on 17/1/17.
+//  Copyright © 2017 Jason Leocata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
